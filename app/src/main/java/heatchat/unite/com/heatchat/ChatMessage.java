eb3395d6d21e05defd4d4201ae3eaa53e959d340
yharaskrik <jaycooperbell@gmail.com>
2017-10-31 22:29:05 -0700

    Initial commit

---
+++ app/src/main/java/heatchat/unite/com/heatchat/ChatMessage.java
@@ -0,0 +1,57 @@
+package heatchat.unite.com.heatchat;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ChatMessage {
+
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        // Initialize to current time
+        this.messageTime = new Date().getTime();
+    }
+
+    public ChatMessage(){
+
+    }
+
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("uid", messageUser);
+        result.put("body", messageText);
+        result.put("time", messageTime);
+
+        return result;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
